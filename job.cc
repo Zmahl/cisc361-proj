c80481e83a809f0a21e3d02b54a13e04acc18628
zirons1 <zirons@udel.edu>
2018-05-07 17:13:04 -0400

    Schtuff

--- job.cc
+++ job.cc
@@ -1,51 +1,34 @@
+#include "job.h"
+
 using namespace std;
 
-class Job{
-public:
-
-	Job(int j, int m, int s,int p, int a, int r, int q, int d){
-		jobnum = j;
-		memory = m;
-		priority = p;
-		arrtime = a;
-		runtime = r;
-		quantum = q;
-		demand = d;
-	}
-
-	int get_jobnum(){
-		return jobnum;
-	}
-	int get_memory(){
-		return memory;
-	}
-	int get_priority(){
-		return priority;
-	}
-	int get_arrtime(){
-		return arrtime;
-	}
-	int get_runtime(){
-		return runtime;
-	}
-	int get_quantum(){
-		return quantum;
-	}
-	int get_demand(){
-		return demand;
-	}
-
-private:
-	int jobnum;
-	int memory;
-	int serial;
-	int priority;
-	int arrtime;
-	int runtime;
-	int quantum;
-	int demand;
-};
-
-int main(){
-
+Job::Job(int j, int m, int s,int p, int a, int r, int q, int d){
+	jobnum = j;
+	memory = m;
+	priority = p;
+	arrtime = a;
+	runtime = r;
+	quantum = q;
+	demand = d;
+}
+int Job::get_jobnum(){
+	return jobnum;
+}
+int Job::get_memory(){
+	return memory;
+}
+int Job::get_priority(){
+	return priority;
+}
+int Job::get_arrtime(){
+	return arrtime;
+}
+int Job::get_runtime(){
+	return runtime;
+}
+int Job::get_quantum(){
+	return quantum;
+}
+int Job::get_demand(){
+	return demand;
 }
---
+++ job.h
@@ -0,0 +1,20 @@
+class Job{
+private:
+	int jobnum;
+	int memory;
+	int serial;
+	int priority;
+	int arrtime;
+	int runtime;
+	int quantum;
+	int demand;
+public:
+	Job(int j, int m, int s,int p, int a, int r, int q, int d);
+	int get_jobnum();
+	int get_memory();
+	int get_priority();
+	int get_arrtime();
+	int get_runtime();
+	int get_quantum();
+	int get_demand();
+};
