c150648046297b0c9df8b0caf182653b6a3f4a84
zirons1 <zirons@udel.edu>
2018-05-07 16:45:10 -0400

    Fixed Job constructor but now weird 'main' class errors

--- job.cc
+++ job.cc
@@ -1,6 +1,39 @@
 using namespace std;
 
-class Job (
+class Job{
+public:
+
+	Job(int j, int m, int s,int p, int a, int r, int q, int d){
+		jobnum = j;
+		memory = m;
+		priority = p;
+		arrtime = a;
+		runtime = r;
+		quantum = q;
+		demand = d;
+	}
+
+	int get_jobnum(){
+		return jobnum;
+	}
+	int get_memory(){
+		return memory;
+	}
+	int get_priority(){
+		return priority;
+	}
+	int get_arrtime(){
+		return arrtime;
+	}
+	int get_runtime(){
+		return runtime;
+	}
+	int get_quantum(){
+		return quantum;
+	}
+	int get_demand(){
+		return demand;
+	}
 
 private:
 	int jobnum;
@@ -11,38 +44,5 @@ private:
 	int runtime;
 	int quantum;
 	int demand;
-public:
-
-	Job(int j, int m, int s,int p, int a, int r, int q, int d){
-		this->jobnum = j;
-		this->memory = m;
-		this->priority = p;
-		this->arrtime = a;
-		this->runtime = r;
-		this->quantum = q;
-		this->demand = d;
-	}
-
-	int get_jobnum(){
-		return this->jobnum;
-	}
-	int get_memory(){
-		return this->memory;
-	}
-	int get_priority(){
-		return this->priority;
-	}
-	int get_arrtime(){
-		return this->arrtime;
-	}
-	int get_runtime(){
-		return this->runtime;
-	}
-	int get_quantum(){
-		return this->quantum;
-	}
-	int get_demand(){
-		return this->demand;
-	}
 };
 
