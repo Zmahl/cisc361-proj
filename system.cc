c1639c6d2dae3f74397ede4f72cca58cd64df584
zirons1 <zirons@udel.edu>
2018-05-08 21:03:50 -0400

    Well we can read a file

--- system.cc
+++ system.cc
@@ -1,19 +1,22 @@
 #include <iostream>
 #include <fstream>
+#include <stdlib.h>
+#include <string>
 using namespace std;
 
 int main(){
-
+	string x;
 	ifstream inFile;
 
-	inFile.open("");
+	inFile.open("test.txt");
 	if (!inFile){
 		cout << "Unable to open file";
 		exit(1);
 	}
 
-	while (inFile >> x){
-
+	while ( getline(inFile, x) ){
+		cout << x << '\n';
+		cout << x[0] << '\n';
 	}
 
 	inFile.close();
